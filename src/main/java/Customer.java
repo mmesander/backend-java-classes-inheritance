60467fdcea1023bfbfff1a49eed6309eac6d360a
Mark Mesander <mark_mesander@hotmail.com>
2023-09-28 12:16:13 +0200

    add opdracht 2

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,27 +1,55 @@
 public class Customer {
-    public String firstName;
-    public String lastName;
-    public int customerNumber;
+    private String firstName;
+    private String lastName;
+    private int customerNumber;
+    private CreditCard creditCard;
 
-    public Customer(String firstName, String lastName, int customerNumber) {
+    public Customer(String firstName, String lastName, int customerNumber, CreditCard creditCard) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.customerNumber = customerNumber;
+        this.creditCard = creditCard;
     }
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, CreditCard creditCard) {
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.creditCard = creditCard;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
 
     public void printName() {
         System.out.println("Customer " + firstName);
     }
-
-
-
-
-
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer = new Customer("Mark", "Mesander", 1);
-        Customer customer2 = new Customer("Emeraude", "Mesander");
+        Customer customer = new Customer("Mark", "Mesander", new CreditCard(100));
+        Customer customer2 = new Customer("Emeraude", "Mesander", new CreditCard(200));
 
         customer.printName();
         customer2.printName();
+
+        customer.getCreditCard().pay(10);
+        System.out.println(customer.getCreditCard().getDebt());
     }
 }
