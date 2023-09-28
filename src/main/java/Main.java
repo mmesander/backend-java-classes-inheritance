1ab9067487fdf320b0c8d6a648bb6dab0c5102ad
Mark Mesander <mark_mesander@hotmail.com>
2023-09-28 12:30:39 +0200

    add opdracht 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         Customer customer = new Customer("Mark", "Mesander", new MasterCard(100));
-        Customer customer2 = new Customer("Emeraude", "Mesander", new VisaCard(200));
+        Customer customer2 = new Customer("Emeraude", "Mesander", new VisaCard(100));
 
         customer.printName();
         customer2.printName();
 
         customer.getCreditCard().pay(10);
+        customer2.getCreditCard().pay(10);
         System.out.println(customer.getCreditCard().getDebt());
+        System.out.println(customer2.getCreditCard().getDebt());
     }
 }
--- src/main/java/VisaCard.java
+++ src/main/java/VisaCard.java
@@ -2,4 +2,9 @@ public class VisaCard extends CreditCard {
     public VisaCard(int debt) {
         super(debt);
     }
+
+    @Override
+    public void pay(int amount) {
+        super.pay((int)(amount * 0.9));
+    }
 }
